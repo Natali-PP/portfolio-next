96c56ef053569ac4d893e82c336e968fed0c82c2
Natali Palacio Pastor <cristinanatalip@gmail.com>
2020-08-01 13:36:34 -0300

    Deleted old gatsby dependencies

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "gatsby"
 //import Layout from "../components/layout"
 import styled from "styled-components"
 //import Cuadrados3D from '../components/Cuadrados3D'
